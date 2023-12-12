156cde2b900205b6c8017633935d3c73b419e2a1
Ajit Pawar <150226246+AjitPawar-Xenpark@users.noreply.github.com>
2023-12-12 18:29:57 +0530

    slider

--- script.js
+++ script.js
@@ -13,3 +13,61 @@ hamburger.addEventListener('click',()=>{
     }
 
 })
+
+
+
+
+
+// const slides=document.querySelectorAll(".slide")
+// var counter=0;
+// /// console.log(slides)
+// slides.forEach(
+//     (slide,index)=>{
+//         slide.style.left=`${index*100}%`
+//     }
+// )
+
+
+// const goNext = () => {
+//     if (counter < slides.length - 1) {
+//     counter++;
+//     slideImage();
+//     }
+//     };
+//     const goPrev = () => {
+//     if (counter != 0) {
+//     counter--;
+//     slideImage();
+//     }
+//     };
+
+// const slideImage=()=>{
+//     slides.forEach(
+//         (slide)=>{
+//             slide.style.transform=`translateX(-${counter*100}%)`
+//         }
+//     )
+// }
+
+//Slider JS Start
+function first(){
+    document.getElementById("slideimage").src="./images/digital-marketing.webp";
+}
+
+
+function second(){
+    document.getElementById("slideimage").src="./images/e-Commerce-development.jpg";
+}
+
+
+function third(){
+    document.getElementById("slideimage").src="./images/Hire_Dedicated_Talent.jpg";
+}
+
+
+
+
+
+setInterval(first,1000);
+setInterval(second,2000);
+setInterval(third,4000);
